ac78e53b92accbdb3b6fafa115934fd6b674ae79
Antonino Ingargiola <tritemio@gmail.com>
2018-08-14 15:56:36 +0200

    CI: finalize py37 support on TravisCI

--- setup.py
+++ setup.py
@@ -39,5 +39,6 @@ setup(
         'Natural Language :: English',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
     ],
 )
